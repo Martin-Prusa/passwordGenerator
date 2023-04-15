0e1ffa32fde95f1c460bb0bcc55e4398b89c3ed4
Martin Průša <prusamartin.pm@gmail.com>
2023-04-15 18:52:26 +0200

    password generation

--- passwordGenerator/passwordGeneratorReducer/passwordGeneratorReducer.ts
+++ passwordGenerator/passwordGeneratorReducer/passwordGeneratorReducer.ts
@@ -1,5 +1,44 @@
 import {Action, PasswordState} from "@/passwordGenerator/types";
 
+
+const characters = {
+    symbols: '+@#$~&*-_><?!'
+}
+const generatePassword = (options: PasswordState): string => {
+    let lowerCount = options.length
+
+    const numbersCount = options.includeNumbers ? Math.ceil(Math.random() * (options.length / 4)) : 0
+    lowerCount -= numbersCount
+
+    const symbolsCount = options.includeSymbols ? Math.ceil(Math.random() * (options.length / 4)) : 0
+    lowerCount -= symbolsCount
+
+    const upperCount = options.includeUppercase ? Math.ceil(Math.random() * (options.length / 4)) : 0
+    lowerCount -= upperCount
+
+    let generatedPassword = []
+
+    for (let i = 0; i < numbersCount; i++) {
+        generatedPassword.push(Math.floor(Math.random()*10))
+    }
+    for (let i = 0; i < symbolsCount; i++) {
+        generatedPassword.push(characters.symbols.at(Math.floor(Math.random()*characters.symbols.length)))
+    }
+    for (let i = 0; i < upperCount; i++) {
+        generatedPassword.push(String.fromCharCode(Math.floor(Math.random()*(90-65))+65))
+    }
+    for (let i = 0; i < lowerCount; i++) {
+        generatedPassword.push(String.fromCharCode(Math.floor(Math.random()*(122-97))+97))
+    }
+
+
+    return generatedPassword
+        .map((value) => ({value, rand: Math.random()}))
+        .sort((i,j) => i.rand - j.rand)
+        .map(i => (i.value))
+        .join('')
+}
+
 export const passwordGeneratorReducer = (state: PasswordState, action: Action): PasswordState => {
     switch (action.type) {
         case "setOptions":
@@ -8,10 +47,9 @@ export const passwordGeneratorReducer = (state: PasswordState, action: Action): 
                 ...action.options
             }
         case "generate":
-            console.log(state)
             return {
                 ...state,
-                value: 'heslo'
+                value: generatePassword(state)
             }
         default:
             return state
